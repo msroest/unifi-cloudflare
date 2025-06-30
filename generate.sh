4d7a405c864bca7f96b93849e88e3c572c87047e
Mike Roest <msroest@gmail.com>
2025-06-29 19:44:50 -0600

    updates

--- generate.sh
+++ generate.sh
@@ -4,8 +4,10 @@ if [ -d /state ]; then
     .venv/bin/python3 generate.py
     pushd terraform
     tofu init
-    tofu plan -out plan 
-    tofu apply plan
+    tofu plan -out plan
+    if [ "$APPLY" == "true" ]; then
+       tofu apply plan
+    fi
     popd
 else
     echo "No state persistance"
